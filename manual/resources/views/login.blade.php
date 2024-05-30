236b29e7248b4be0c6e1d58fc0b25519855b9688
asuasuasuasuasuasu <a.nakamur@so-ra.co.jp>
2024-05-30 11:32:23 +0900

    ログイン画面登録画面css

--- manual/resources/views/login.blade.php
+++ manual/resources/views/login.blade.php
@@ -5,27 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>login</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
+
 <body>
-    <h1>ログイン画面</h1>
-    <form action="{{ route('login') }}" method="post">
-        @csrf
-        <label for="email">メールアドレス</label>
-        <input name="email" id="email">
-        <label for="password">パスワード</label>
-        <input type="password" name="password" id="password">
-        <button type="submit">送信</button>
-    </form>
+    <div class="login">
+        <div class="login-screen">
+            <div class="app-title">
+                <h1>ログイン</h1>
+            </div>
 
-    @if ($errors->any())
-        <div>
-            @foreach ($errors->all() as $error)
-            {{ $error }}
-            @endforeach
+            <div class="login-form">
+                <div class="control-group">    
+                    <form action="{{ route('login') }}" method="post">
+                        @csrf
+                        <label for="email">メールアドレス</label>
+                        <input name="email" id="email">
+                        <label for="password">パスワード</label>
+                        <input type="password" name="password" id="password">
+                        <button type="submit" class="btn btn-primary btn-large btn-block">ログイン</button>
+                    </form> 
+                    
+                        @if ($errors->any())
+                            <div>
+                                @foreach ($errors->all() as $error)
+                                {{ $error }}
+                                @endforeach
+                            </div>
+                        @endif                       
+                        <a href="{{ route('register') }}">会員登録はこちら</a>                   
+                </div>
+            </div>
         </div>
-    @endif
-
-    <a href="{{ route('register') }}">会員登録はこちら</a>
-
+    </div>
 </body>
 </html>
--- manual/resources/views/register.blade.php
+++ manual/resources/views/register.blade.php
@@ -6,31 +6,39 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>登録画面</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
+
 <body>
-    <h1>登録画面</h1>
-    <form action="{{ route('register') }}" method="post">
-        @csrf
-        <label for="name">名前</label>
-        <input type="text" name="name" id="name"><br>
-            @error('name')
-            <span>{{ $message }}</span>
-            @enderror
-        <br>
-        <label for="email">メールアドレス</label>
-        <input name="email" id="email"><br>
-            @error('email')
-            <span>{{ $message }}</span>
-            @enderror
-        <br>
-        <label for="password">パスワード</label>
-        <input type="password" name="password" id="password"><br>
-            @error('password')
-            <span>{{ $message }}</span>
-            @enderror
-            <br>
-        <button type="submit">登録</button>
-        <input type="button" value="戻る" onclick="window.history.back()">
-    </form>
+    <div class="login">
+        <div class="login-screen">
+        <div class="app-title">
+            <h1>会員登録</h1>
+        </div>   
+        <div class="login-form">
+            <div class="control-group">    
+                <form action="{{ route('register') }}" method="post">
+                    @csrf
+                    <label for="name">名前</label>
+                    <input type="text" name="name" id="name"><br>
+                        @error('name')
+                        <span>{{ $message }}</span>
+                        @enderror
+                    <label for="email">メールアドレス</label>
+                    <input name="email" id="email">
+                        @error('email')
+                        <span>{{ $message }}</span>
+                        @enderror
+                    <label for="password">パスワード</label>
+                    <input type="password" name="password" id="password">
+                        @error('password')
+                        <span>{{ $message }}</span>
+                        @enderror
+                    <button type="submit" class="btn btn-primary btn-large btn-block">登録</button>
+                </form> 
+                <a href="{{ route('login') }}">ログインはこちら</a>   
+            </div>
+        </div>
+    </div>
 </body>
 </html>
